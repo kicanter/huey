552cfec09de304437bf3e9dc62c9d1dfa0dbd706
Kieran Canter <canter.kieran@gmail.com>
2026-07-10 13:39:01 -0400

    Trying to follow `fzf` usage conventions
    Split method of display up between alt screen (fullscreen experience)
    and primary screen (inline below prompt). Default is fullscren with an
    ultimate height flag determining height of the TUI, where 0 and 100 just
    mean fullscreen as well.

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,35 @@ use ratatui::{
 
 const HEIGHT: u16 = 8;
 
-fn main() -> io::Result<()> {
+// Runs in alternate screen for fullscreen TUI experience
+fn run_fullscreen() -> io::Result<()> {
+    ratatui::run(|terminal| -> io::Result<()> {
+        let mut should_quit = false;
+        while !should_quit {
+            terminal.draw(|frame| {
+                let area = frame.area();
+                // Match background color of terminal by not setting a color
+                let block = Block::bordered();
+                frame.render_widget(block, area);
+            })?;
+
+            if let Event::Key(key) = event::read()? {
+                match key.kind {
+                    KeyEventKind::Press => match key.code {
+                        KeyCode::Char('q') | KeyCode::Esc => should_quit = true,
+                        _ => {}
+                    },
+                    _ => {}
+                }
+            }
+        }
+        Ok(())
+    })?;
+    Ok(())
+}
+
+// Runs in primary screen for an inline experience
+fn run_inline() -> io::Result<()> {
     std::panic::set_hook(Box::new(|info| {
         let _ = disable_raw_mode();
         eprintln!("{info}");
@@ -52,4 +80,13 @@ fn main() -> io::Result<()> {
     terminal.clear()?;
     disable_raw_mode()?;
     Ok(())
+}
+
+fn main() -> io::Result<()> {
+    // HACK: this will be a flag or something at some point to decide between alt or primary screen
+    let height: Option<u8> = Some(0);
+    match height {
+        Some(h) if 0 < h && h < 100 => run_inline(),
+        _ => run_fullscreen(),
+    }
 }
